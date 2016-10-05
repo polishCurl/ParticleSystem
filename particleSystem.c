9bf40590dce9431291714d42519852176eaec466
Krzysztof Koch <krzysztofkoch@Krzysztofs-MBP.home>
2016-10-05 22:59:47 +0100

    Extra stuff left to do

--- particleSystem.c
+++ particleSystem.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 void initGraphics(int argc, char *argv[])
 {
   glutInit(&argc, argv);
-  glutInitWindowSize(800, 600);
+  glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
   glutInitWindowPosition(100, 100);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
   glutCreateWindow("COMP37111 Fountain");
@@ -51,11 +51,12 @@ void display()
             0.0, 1.0, 0.0);
   
   glClear(GL_COLOR_BUFFER_BIT);  // Clear the screen and depth buffer
-  if(AXIS_ENABLED) glCallList(axisList);    // If enabled, draw coordinate axis
+  if(axisEnabled) glCallList(axisList);    // If enabled, draw coordinate axis
   spawnParticles();
   drawParticles();
   progressTime();
   glutPostRedisplay();
+  calculateFPS();
   glutSwapBuffers();                        // Double buffering in place
 }
 
@@ -66,16 +67,17 @@ void display()
 void spawnParticles() 
 {
   int index;
-  for (index = 0; index < NO_OF_WATERDROPS; index++) 
+
+  for (index = 0; index < noOfWaterdrops; index++) 
   {
     if (water[index].alive == 0) 
     {
       water[index].xpos = FOUNTAIN_X;
       water[index].ypos = FOUNTAIN_Y;
       water[index].zpos = FOUNTAIN_Z;
-      water[index].xvel = myRandom(SIDE_SPLASH_MAX);
-      water[index].yvel = myRandom(WATER_SPEED_MAX);
-      water[index].zvel = myRandom(SIDE_SPLASH_MAX);
+      water[index].xvel = gaussianRandom(0.0, SIDE_SPLASH_VAR);
+      water[index].yvel = gaussianRandom(WATER_SPEED_MEAN, WATER_SPEED_VAR);
+      water[index].zvel = gaussianRandom(0.0, SIDE_SPLASH_VAR);
       water[index].r = WATER_DROP_COLOUR_R;
       water[index].g = WATER_DROP_COLOUR_G;
       water[index].b = WATER_DROP_COLOUR_B;
@@ -83,6 +85,25 @@ void spawnParticles()
       water[index].mass = WATER_DROP_MASS;
     }
   }
+
+  for (index = 0; index < noOfSmokeParticles; index++) 
+  {
+    if (smoke[index].alive <= 0) 
+    {
+      smoke[index].xpos = gaussianRandom(SMOKE_EMITTER_X, SMOKE_EMITTER_SIZE);
+      smoke[index].ypos = SMOKE_EMITTER_Y;
+      smoke[index].zpos = gaussianRandom(SMOKE_EMITTER_Z, SMOKE_EMITTER_SIZE);
+      smoke[index].xvel = 0.0;
+      smoke[index].yvel = gaussianRandom(SMOKE_SPEED_MEAN, SMOKE_SPEED_VAR);
+      smoke[index].zvel = 0.0;
+      smoke[index].r = smokeR;
+      smoke[index].g = smokeG;
+      smoke[index].b = smokeB;
+      smoke[index].alive = (int) gaussianRandom(SMOKE_PARTICLE_LIFETIME_MEAN, SMOKE_PARTICLE_LIFETIME_STD);
+      smoke[index].mass = SMOKE_PARTICLE_MASS;
+    }
+  }
+
 }
 
 
@@ -92,13 +113,22 @@ void spawnParticles()
 void drawParticles() 
 {
   int index;
+
   glBegin (GL_POINTS);
-  for (index = 0; index < NO_OF_WATERDROPS; index++) 
+  // Draw the fountain
+  for (index = 0; index < noOfWaterdrops; index++) 
   {
     glColor3f(water[index].r , water[index].g, water[index].b);
     glVertex3f(water[index].xpos, water[index].ypos, water[index].zpos);
   }
-  glEnd ();
+
+  // Draw the smoke
+  for (index = 0; index < noOfSmokeParticles; index++) 
+  {
+    glColor3f(smoke[index].r , smoke[index].g, smoke[index].b);
+    glVertex3f(smoke[index].xpos, smoke[index].ypos, smoke[index].zpos);
+  }
+  glEnd();
 }
 
 
@@ -108,14 +138,43 @@ void drawParticles()
 void progressTime() 
 {
   int index;
-  for (index = 0; index < NO_OF_WATERDROPS; index++) 
+  double shadeChange;
+
+  // Update each water particle parameters. Water particles maintain
+  // their X and Z speeds while the vertical keeps being modified due to
+  // gravity
+  for (index = 0; index < noOfWaterdrops; index++) 
   {
     water[index].xpos += water[index].xvel;
     water[index].ypos += water[index].yvel;
     water[index].zpos += water[index].zvel;
-    water[index].yvel += water[index].mass * GRAVITY;
-    if (water[index].ypos < FOUNTAIN_Y)
+    water[index].yvel += water[index].mass * gravity;
+    // if particle falls below the fountain position it is killed
+    if (water[index].ypos < FOUNTAIN_Y || water[index].ypos > WINDOW_HEIGHT)
       water[index].alive = 0;
+  }
+
+  // Update each smoke particle parameters. 
+  for (index = 0; index < noOfSmokeParticles; index++) 
+  {
+    smoke[index].xpos += smoke[index].xvel;
+    smoke[index].ypos += smoke[index].yvel;
+    smoke[index].zpos += smoke[index].zvel;
+    // Apart from minor gravitational force each particle has some chaotic 
+    // movement in every dimension
+    smoke[index].xvel += gaussianRandom(SMOKE_CHAOS_SPEED_MEAN, chaoticSpeed);
+    smoke[index].yvel += smoke[index].mass * gravity + gaussianRandom(SMOKE_CHAOS_SPEED_MEAN, chaoticSpeed);
+    smoke[index].zvel += gaussianRandom(SMOKE_CHAOS_SPEED_MEAN, chaoticSpeed);
+    // Each particle fades away at slighlty different pace
+    shadeChange = gaussianRandom(SMOKE_SHADE_CHANGE_MEAN, SMOKE_SHADE_CHANGE_VAR);
+    smoke[index].r -= shadeChange;
+    smoke[index].g -= shadeChange;
+    smoke[index].b -= shadeChange;
+    // Count down to particle death, he he 
+    smoke[index].alive -= 1;
+    // If smoke hits the ground, make it crawl on it
+    if (smoke[index].ypos < SMOKE_EMITTER_Y)
+      smoke[index].ypos = SMOKE_EMITTER_Y;
   }
 }
 
@@ -126,13 +185,55 @@ void progressTime()
 **********************************************************************/
 void keyboard(unsigned char key, int x, int y)
 {
-  if(key == 27) exit(0);
+  switch(key) 
+  {
+    case 27: exit(0); break;
+    case 'f': gravity += 0.01; break;
+    case 'F': gravity -= 0.01; break; 
+    case 'c': if (chaoticSpeed - 0.001 > 0.0)
+                chaoticSpeed -= 0.001;
+              break; 
+    case 'C': chaoticSpeed += 0.001; break; 
+    case 'w': if (noOfWaterdrops / 2 >= 1)
+                noOfWaterdrops /= 2; 
+              break;
+    case 'W': noOfWaterdrops *= 2; break;
+    case 's': if (noOfSmokeParticles / 2 >= 1)
+                noOfSmokeParticles /= 2; 
+              break;
+    case 'S': noOfSmokeParticles *= 2; break;
+    case 'r': if (smokeR - 0.05 >= 0.0)
+                smokeR -= 0.05; 
+              break;
+    case 'R': if (smokeR + 0.05 <= 1.0)
+                smokeR += 0.05; 
+              break;
+    case 'g': if (smokeG - 0.05 >= 0.0)
+                smokeG -= 0.05; 
+              break;
+    case 'G': if (smokeG + 0.05 <= 1.0)
+                smokeG += 0.05; 
+              break;
+    case 'b': if (smokeB - 0.05 >= 0.0)
+                smokeB -= 0.05; 
+              break;
+    case 'B': if (smokeB - 0.05 >= 0.0)
+                smokeB -= 0.05; 
+              break;
+    case 'e': gravity = DEFAULT_GRAVITY;
+              noOfWaterdrops = DEFAULT_NO_OF_WATERDROPS;
+              noOfSmokeParticles = DEFAULT_NO_OF_SMOKE_PARTICLES;
+              chaoticSpeed = SMOKE_CHAOS_SPEED_VAR;
+              smokeR = smokeG = smokeB = SMOKE_SHADE;
+              break;
+
+  }
   glutPostRedisplay();
 }
 
 
 /*********************************************************************
-* Reshape function called whenever the application windos is reshaped
+* Reshape function called whenever the application window is reshaped
 **********************************************************************/
 void reshape(int width, int height)
 {
@@ -168,66 +269,66 @@ void makeAxes()
 }
 
 
-
-
-
 /*********************************************************************
-* Return random double within range [-range,range]
+* Return uniformly distributed random double within range [-range,range]
 **********************************************************************/
-double myRandom(double range)
+double uniformRandom(double range)
 {
-  return (rand() / (double)RAND_MAX * 2.0 - 1.0) * range;
+  return (rand() / (double)RAND_MAX * 2.0 - 1.0) * range; 
 }
 
 
+/*********************************************************************
+* Return gaussian random variable with mean "mean" and standard 
+* deviation "stdDev". Uses to uniform random variables for generation of
+* a normally distributed one
+**********************************************************************/
+double gaussianRandom(double mean, double stdDev)
+{
+  double uniform1, uniform2, w;
+ 
+  do {
+    uniform1 = uniformRandom(1.0);
+    uniform2 = uniformRandom(1.0);
+    w = uniform1 * uniform1 + uniform2 * uniform2;
+  } while ( w >= 1.0 );
+
+  w = sqrt((-2.0 * log(w)) / w);
+  return uniform1 * w * stdDev + mean;
+}
+
 
 /*********************************************************************
-* Draw the fountain from which the water will originate
-**********************************************************************
-void drawFountain()
+* Calculate the number of frames per second
+**********************************************************************/
+void calculateFPS()
 {
-  glPushMatrix();
-    glColor3f(0.7, 0.7, 0.7);
-    glBegin(GL_POLYGON);
-      glVertex3f(  50, -50, -50 );      
-      glVertex3f(  50,  50, -50 );   
-      glVertex3f( -50,  50, -50 );      
-      glVertex3f( -50, -50, -50 );   
-    glEnd();
+  //  Increase frame count
+  frameCount++;
 
-    glBegin(GL_POLYGON);
-      glVertex3f(  50, -50, 50 );
-      glVertex3f(  50,  50, 50 );
-      glVertex3f( -50,  50, 50 );
-      glVertex3f( -50, -50, 50 );
-    glEnd();
- 
-    glBegin(GL_POLYGON);
-      glVertex3f( 50, -50, -50 );
-      glVertex3f( 50,  50, -50 );
-      glVertex3f( 50,  50,  50 );
-      glVertex3f( 50, -50,  50 );
-    glEnd();
- 
-    glBegin(GL_POLYGON);
-      glVertex3f( -50, -50,  50 );
-      glVertex3f( -50,  50,  50 );
-      glVertex3f( -50,  50, -50 );
-      glVertex3f( -50, -50, -50 );
-    glEnd();
- 
-    glBegin(GL_POLYGON);
-      glVertex3f(  50,  50,  50 );
-      glVertex3f(  50,  50, -50 );
-      glVertex3f( -50,  50, -50 );
-      glVertex3f( -50,  50,  50 );
-    glEnd();
- 
-    glBegin(GL_POLYGON);
-      glVertex3f(  50, -50, -50 );
-      glVertex3f(  50, -50,  50 );
-      glVertex3f( -50, -50,  50 );
-      glVertex3f( -50, -50, -50 );
-    glEnd();
-  glPopMatrix();
-}*/
+  //  Get the number of milliseconds since glutInit called (or first call
+  //  to glutGet(GLUT ELAPSED TIME)) and calculate time passed
+  currentTime = glutGet(GLUT_ELAPSED_TIME);
+  int timeInterval = currentTime - previousTime;
+
+  if(timeInterval > 1000)
+  {
+    fps = frameCount / (timeInterval / 1000.0f);  //  calculate the number of frames per second
+    previousTime = currentTime;                   //  Set time
+    frameCount = 0;                               //  Reset frame count       
+  }
+}
+
+
+/*********************************************************************
+* Draw string ’str’ in font ’font’, at world (x,y,0)
+**********************************************************************/
+void drawString (void* font, float x, float y, char* str) 
+{
+  char* ch;
+  glRasterPos3f(x, y, 0.0);
+  for (ch = str; *ch; ch++)
+    glutBitmapCharacter(font, (int) *ch);
+}
+
+
--- particleSystem.h
+++ particleSystem.h
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <math.h>
+#include <float.h>
 
 #ifdef MACOSX
   #include <GLUT/glut.h>
@@ -25,16 +26,21 @@
 // Display list for coordinate axis 
 GLuint axisList;
 
+// Variables for calculating FPS
+int frameCount = 0;
+int currentTime, previousTime;
+double fps;
+
 
 /*********************************************************************
 * Particle definition
 **********************************************************************/
 typedef struct {
-  GLdouble xpos, ypos, zpos;   	// position
-  GLdouble xvel, yvel, zvel;   	// velocity
-  GLdouble r, g, b;   			// color
-  GLdouble alive;        	   	// 0 - dead, nonzero - alive
-  GLdouble mass; 				// particle mass
+  GLdouble xpos, ypos, zpos;   			// position
+  GLdouble xvel, yvel, zvel;   			// velocity
+  GLdouble r, g, b;   					// color
+  int alive;        	   				// 0 - dead, nonzero - alive
+  double mass; 							// particle mass
 } Particle;
 
 
@@ -46,15 +52,18 @@ typedef struct {
 #define POINT_SIZE 1					// radius of point in pixels
 #define EYE_X 0.0						// camera position
 #define EYE_Y 0.0
-#define EYE_Z 300.0
+#define EYE_Z 500.0
 #define CENTER_X 0.0					// point at which camera is pointing
 #define CENTER_Y 0.0
 #define CENTER_Z 0.0
+#define WINDOW_WIDTH 1200				// window size
+#define WINDOW_HEIGHT 800
+#define DEFAULT_GRAVITY -0.03;			// default gravitational acceleration
+#define AXIS_SIZE 400
 
-// Variable parameters
-int AXIS_SIZE = 400; 			     
-int AXIS_ENABLED = 0;
-double GRAVITY = -0.01;
+// Variable parameters			     
+int axisEnabled = 0;
+double gravity = DEFAULT_GRAVITY;
 
 
 
@@ -63,20 +72,21 @@ double GRAVITY = -0.01;
 **********************************************************************/
 
 // Constant parameters
-#define FOUNTAIN_X -120.0 				// fountain location
-#define FOUNTAIN_Y -150.0
+#define FOUNTAIN_X -220.0 				// fountain location
+#define FOUNTAIN_Y -240.0
 #define FOUNTAIN_Z 0.0
-#define DEFAULT_NO_OF_WATERDROPS 1000	
-#define MAX_NO_OF_WATERDROPS 100000
-#define WATER_SPEED_MAX 4.5				// max initial vertical water speed
-#define SIDE_SPLASH_MAX 0.6				// max distance of side water splash
-#define WATER_DROP_COLOUR_R 0.0			// particle RGB colour 
-#define WATER_DROP_COLOUR_G 0.0
+#define DEFAULT_NO_OF_WATERDROPS 1000
+#define MAX_NO_OF_WATERDROPS 1000000		
+#define WATER_SPEED_MEAN 11.0			// mean initial vertical velocity 
+#define WATER_SPEED_VAR 1.2				// variance of initial vertical speed
+#define SIDE_SPLASH_VAR 0.25			// max distance of side water splash
+#define WATER_DROP_COLOUR_R 0.36		// water particle RGB colour 
+#define WATER_DROP_COLOUR_G 0.71
 #define WATER_DROP_COLOUR_B 1.0
-#define WATER_DROP_MASS 10.0			// affects the impact of gravity
+#define WATER_DROP_MASS 9.0				// particle mass, affects the impact of gravity
 
 // Variable parameters
-int NO_OF_WATERDROPS = DEFAULT_NO_OF_WATERDROPS;
+int noOfWaterdrops = DEFAULT_NO_OF_WATERDROPS;
 
 // Water
 Particle water[MAX_NO_OF_WATERDROPS];
@@ -88,32 +98,48 @@ Particle water[MAX_NO_OF_WATERDROPS];
 **********************************************************************/
 
 // Constant parameters
-#define SMOKE_EMITTER_X_MIN 110.0
-#define SMOKE_EMITTER_X_MAX 130.0 	
-#define SMOKE_EMITTER_Y -150.0
-#define SMOKE_EMITTER_Z_MIN  -10.0	
-#define SMOKE_EMITTER_Z_MAX -10.0	 	
-#define MAX_NO_OF_SMOKE_PARTICLES 100000
-#define DEFAULT_NO_OF_SMOKE_PARTICLES 1000	
+#define SMOKE_EMITTER_X 220.0			// smoke emitter location
+#define SMOKE_EMITTER_Y -240.0
+#define SMOKE_EMITTER_Z 0.0
+#define SMOKE_EMITTER_SIZE 10.0			// variance of possible particle spawn locations
+#define DEFAULT_NO_OF_SMOKE_PARTICLES 1000	 	
+#define SMOKE_SPEED_MEAN 0.7			// starting vertical smoke speed		
+#define SMOKE_SPEED_VAR 0.1				
+#define SMOKE_CHAOS_SPEED_MEAN 0.0		// velocity of particle chaotic movement
+#define SMOKE_CHAOS_SPEED_VAR 0.008		
+#define SMOKE_SHADE 0.8					// initial colour of smoke (gray)
+#define SMOKE_SHADE_CHANGE_MEAN 0.001	// rate of smoke shade change
+#define SMOKE_SHADE_CHANGE_VAR 0.01		
+#define SMOKE_PARTICLE_MASS 0.015		// affects the impact of gravity
+#define SMOKE_PARTICLE_LIFETIME_MEAN 400.0	// particle lifetime before it's removed	
+#define SMOKE_PARTICLE_LIFETIME_STD 200.0
+
 
 // Variable parameters
-int NO_OF_SMOKE_PARTICLES = DEFAULT_NO_OF_SMOKE_PARTICLES;
+int noOfSmokeParticles = DEFAULT_NO_OF_SMOKE_PARTICLES;	// current number of particles
+GLdouble smokeR = SMOKE_SHADE;			// smoke initial colour
+GLdouble smokeG = SMOKE_SHADE;
+GLdouble smokeB = SMOKE_SHADE;
+GLdouble chaoticSpeed = SMOKE_CHAOS_SPEED_VAR;
 
-// Water
-Particle water[MAX_NO_OF_WATERDROPS];
+// Smoke
+Particle smoke[MAX_NO_OF_WATERDROPS];
 
 
 
 /*********************************************************************
 * Function prototypes
 **********************************************************************/
-double myRandom(double);		// Return random double within range [-range,range]
-void spawnParticles(void); 		// Spawn particles 
-void drawParticles(void); 		// Render particles
-void progressTime(void); 		// update particle parameters according to the laws 
-void display(void); 			// openGL callback function
+double uniformRandom(double);			// uniform random variable generator
+double gaussianRandom(double, double);	// gaussian random variable generator
+void spawnParticles(void); 				// spawn particles 
+void drawParticles(void); 				// render particles
+void progressTime(void); 				// update particle parameters according to the laws 
+void display(void); 					// openGL callback function
 void keyboard(unsigned char, int, int); // keyboard callback function
-void reshape(int, int); 		// window reshape callback function
-void makeAxes(void);			// create a display list for drawing coord axis 
+void reshape(int, int); 				// window reshape callback function
+void makeAxes(void);					// create a display list for drawing coord axis 
 void initGraphics(int, char *argv[]); 	// initialisation function
+void calculateFPS(void); 				// calculate the number of frames per second
+void drawString (void*, float, float, char*); // draw string on screen
 
